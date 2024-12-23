56ffaa1b0f1d8f87e03d69d5661415707fcf1804
ikealmighty <isaacyates7@gmail.com>
2024-12-23 16:19:25 -0600

    added license explanation

--- index.js
+++ index.js
@@ -20,6 +20,21 @@ function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => err && console.log(err))
 }
 
+function getLicenseExplanation(license) {
+    switch (license) {
+        case 'GNU AGPLv3':
+            return 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.'
+        case 'GNU GPLv3':
+            return 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.'
+        case 'Mozilla Public License 2.0':
+            return 'Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.'
+        case 'MIT License':
+            return 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
+        case 'The Unlicense':
+            return 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.'
+    }
+}
+
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then(answers => {
@@ -50,10 +65,12 @@ ${description}\n\n
 ${installationInstructions}\n
 ## Contribute\n
 ${contributionGuidelines}\n\n
-## How to Use
+## How to Use\n
 ${usageInstructions}\n\n
 ## Testing Instructions\n
 ${testingInstructions}\n\n
+## License\n
+${getLicenseExplanation(license)}
 ## Questions\n
 You can send questions to the github account: ${githubUsername},
 or email at ${email}`;
