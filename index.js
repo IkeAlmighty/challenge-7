27128baf6e0a31d314ea0c321325943a8581412d
ikealmighty <isaacyates7@gmail.com>
2024-12-26 12:46:51 -0600

    bugfix: contribute and usage were switched in the prompt instructions

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
-import fs from 'fs';
+import fs from "fs";
 
 // TODO: Create an array of questions for user input
 const questions = [
-    { name: 'title', message: 'What is the title of you project?' },
-    { name: 'description', message: 'Enter the description of your project: ' },
-    { name: 'installationInstructions', message: 'Enter the installation instructions: ' },
-    { name: 'contributionGuidelines', message: 'Enter the usage information: ' },
-    { name: 'usageInstructions', message: 'Enter the contribution guidelines: ' },
-    { name: 'testingInstructions', message: 'Enter instructions for testing: ' },
-    { name: 'license', type: 'list', message: 'Choose a license: ', choices: ['GNU AGPLv3', 'GNU GPLv3', 'Mozilla Public License 2.0', 'MIT License', 'The Unlicense'] },
-    { name: 'githubUsername', message: 'Github Username: ' },
-    { name: 'email', message: 'Email: ' }
+    { name: "title", message: "What is the title of you project?" },
+    { name: "description", message: "Enter the description of your project: " },
+    {
+        name: "installationInstructions",
+        message: "Enter the installation instructions: ",
+    },
+    {
+        name: "contributionGuidelines",
+        message: "Enter the contribution guidelines: ",
+    },
+    { name: "usageInstructions", message: "Enter the usage information: " },
+    {
+        name: "testingInstructions",
+        message: "Enter instructions for testing: ",
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "Choose a license: ",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "Mozilla Public License 2.0",
+            "MIT License",
+            "The Unlicense",
+        ],
+    },
+    { name: "githubUsername", message: "Github Username: " },
+    { name: "email", message: "Email: " },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => err && console.log(err))
+    fs.writeFile(fileName, data, (err) => err && console.log(err));
 }
 
 function getLicenseExplanation(license) {
     switch (license) {
-        case 'GNU AGPLv3':
-            return 'Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.'
-        case 'GNU GPLv3':
-            return 'Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.'
-        case 'Mozilla Public License 2.0':
-            return 'Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.'
-        case 'MIT License':
-            return 'A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.'
-        case 'The Unlicense':
-            return 'A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.'
+        case "GNU AGPLv3":
+            return "Permissions of this strongest copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. When a modified version is used to provide a service over a network, the complete source code of the modified version must be made available.";
+        case "GNU GPLv3":
+            return "Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.";
+        case "Mozilla Public License 2.0":
+            return "Permissions of this copyleft license are conditioned on making available complete source code of licensed works and modifications under the same license or the GNU GPLv3. Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work through interfaces provided by the licensed work may be distributed under different terms and without source code for the larger work.";
+        case "MIT License":
+            return "A short and simple permissive license with conditions only requiring preservation of copyright and license notices. Licensed works, modifications, and larger works may be distributed under different terms and without source code.";
+        case "The Unlicense":
+            return "A license with no conditions whatsoever which dedicates works to the public domain. Unlicensed works, modifications, and larger works may be distributed under different terms and without source code.";
     }
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(answers => {
+    inquirer.prompt(questions).then((answers) => {
         const {
             title,
             license,
@@ -47,7 +67,8 @@ function init() {
             usageInstructions,
             testingInstructions,
             githubUsername,
-            email } = answers;
+            email,
+        } = answers;
 
         const readme = `
 # ${title}\n
@@ -76,7 +97,7 @@ ${getLicenseExplanation(license)}
 You can send questions to the github account: ${githubUsername},
 or email at ${email}`;
 
-        writeToFile('README.md', readme)
+        writeToFile("README.md", readme);
     });
 }
 
